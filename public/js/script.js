b5690d5cfd7cf0592d6e46eda5e3eba67466eb6d
Ronaldo Lopes <innovatecodes@gmail.com>
2025-01-07 22:31:03 -0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -4,16 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const property = {
         year: date.getFullYear(),
         month: (date.getMonth() + 1).toString().padStart(2, "0"),
-        day: date.getDate().toString().padStart(2, "0"),
-        hours: date.getHours().toString().padStart(2, "0"),
-        minutes: date.getMinutes().toString().padStart(2, "0"),
-        seconds: date.getSeconds().toString().padStart(2, "0"),
+        day: date.getDate().toString().padStart(2, "0")
     }
-    const formatDate = () => `${property.year}-${property.month}-${property.day} ${property.hours}:${property.minutes}:${property.seconds}`; // Output => 'YYYY-MM-DD HH:mm:ss'
+    const formatDate = () => `${property.year}-${property.month}-${property.day}`; // Output => 'YYYY-MM-DD'
 
-    createAt.textContent = new Date("2024-12-10 19:37:37").toLocaleString();
+    createAt.textContent = new Date("2024-12-10").toLocaleDateString();
     document.getElementById('year').textContent = 2024;
-    document.getElementById('updatedAt').textContent = new Date("2025-01-07 21:52:24"/*formatDate() | date.getTime()*/).toLocaleString();
+    document.getElementById('updatedAt').textContent = new Date("2025-01-07"/*formatDate() | date.getTime()*/).toLocaleDateString();
 
     if (property.year > 2024) document.getElementById('nextYear').textContent = ` - ${property.year}`;
 })
